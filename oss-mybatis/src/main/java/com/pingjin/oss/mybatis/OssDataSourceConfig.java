aa85115f7c88f08e3621efae9317c9dcb8bc75aa
anthonyzero <736252868@qq.com>
2020-08-11 18:43:29 +0800

    commit

---
+++ oss-mybatis/src/main/java/com/pingjin/oss/mybatis/OssDataSourceConfig.java
@@ -0,0 +1,74 @@
+package com.pingjin.oss.mybatis;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.ResourceLoader;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * 数据源配置
+ */
+@Configuration
+@MapperScan(basePackages = OssDataSourceConfig.PACKAGE,
+    sqlSessionFactoryRef = "OssSqlSessionFactory")
+public class OssDataSourceConfig {
+
+  static final String PACKAGE = "com.pingjin.oss.**";
+
+  /**
+   * ossDataSource.
+   *
+   * @return DataSource DataSource
+   * @throws IOException IOException
+   */
+  @Bean(name = "OssDataSource")
+  @Primary
+  public DataSource ossDataSource() throws IOException {
+      //获取数据源相关信息
+      ResourceLoader loader = new DefaultResourceLoader();
+      InputStream inputStream = loader.getResource("classpath:application.properties")
+          .getInputStream();
+      Properties properties = new Properties();
+      properties.load(inputStream);
+      Set<Object> keys = properties.keySet();
+      Properties dsproperties = new Properties();
+      for (Object key : keys) {
+          if (key.toString().startsWith("datasource")) {
+              dsproperties.put(key.toString().replace("datasource.", ""), properties.get(key));
+          }
+      }
+      //通过HikariDataSourceFactory 构建数据源
+      HikariDataSourceFactory factory = new HikariDataSourceFactory();
+      factory.setProperties(dsproperties);
+      inputStream.close();
+      return factory.getDataSource();
+  }
+
+  /**
+   * OssSqlSessionFactory.
+   */
+  @Bean(name = "OssSqlSessionFactory")
+  @Primary
+  public SqlSessionFactory ossSqlSessionFactory(
+      @Qualifier("OssDataSource") DataSource phoenixDataSource) throws Exception {
+      SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+      factoryBean.setDataSource(phoenixDataSource);
+      ResourceLoader loader = new DefaultResourceLoader();
+      String resource = "classpath:mybatis-config.xml";
+      factoryBean.setConfigLocation(loader.getResource(resource));
+      factoryBean.setSqlSessionFactoryBuilder(new SqlSessionFactoryBuilder());
+      return factoryBean.getObject();
+  }
+}
---
+++ oss-mybatis/src/test/java/com/pingjin/oss/mybatis/test/BaseTest.java
@@ -0,0 +1,21 @@
+package com.pingjin.oss.mybatis.test;
+
+import com.pingjin.oss.mybatis.OssDataSourceConfig;
+import org.junit.runner.RunWith;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Test基类
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@Import(OssDataSourceConfig.class)
+@PropertySource("classpath:application.properties")
+@ComponentScan("com.pingjin.oss.*")
+@MapperScan("com.pingjin.oss.*")
+public class BaseTest {
+
+}
