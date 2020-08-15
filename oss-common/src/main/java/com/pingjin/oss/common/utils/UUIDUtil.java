d5bdf183747c374be5a3ba90dc97d06f38615ac7
anthonyzero <736252868@qq.com>
2020-08-15 15:54:43 +0800

    commit

---
+++ oss-common/src/main/java/com/pingjin/oss/common/utils/UUIDUtil.java
@@ -0,0 +1,10 @@
+package com.pingjin.oss.common.utils;
+
+import java.util.UUID;
+
+public class UUIDUtil {
+
+    public static String getUUID() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+}
