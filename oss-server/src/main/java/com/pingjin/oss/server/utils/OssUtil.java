f119dae1ef06266f0067542777387ffba7b66a0a
anthonyzero <736252868@qq.com>
2020-08-27 18:43:45 +0800

    commit

--- oss-server/src/main/java/com/pingjin/oss/server/utils/OssUtil.java
+++ oss-server/src/main/java/com/pingjin/oss/server/utils/OssUtil.java
@@ -10,46 +10,47 @@ import org.apache.hadoop.hbase.util.Bytes;
 
 public class OssUtil {
 
-    static final byte[][] OBJ_REGIONS = new byte[][]{
+    //预分区
+    public static final byte[][] OBJ_REGIONS = new byte[][]{
         Bytes.toBytes("1"),
         Bytes.toBytes("4"),
         Bytes.toBytes("7")
     };
 
     //表前缀
-    static final String DIR_TABLE_PREFIX = "oss_dir_";
-    static final String OBJ_TABLE_PREFIX = "oss_obj_";
+    public static final String DIR_TABLE_PREFIX = "oss_dir_";
+    public static final String OBJ_TABLE_PREFIX = "oss_obj_";
 
     //目录表列族
-    static final String DIR_META_CF = "cf";
-    static final byte[] DIR_META_CF_BYTES = DIR_META_CF.getBytes();
-    static final String DIR_SUBDIR_CF = "sub";
-    static final byte[] DIR_SUBDIR_CF_BYTES = DIR_SUBDIR_CF.getBytes();
+    public static final String DIR_META_CF = "cf";
+    public static final byte[] DIR_META_CF_BYTES = DIR_META_CF.getBytes();
+    public static final String DIR_SUBDIR_CF = "sub";
+    public static final byte[] DIR_SUBDIR_CF_BYTES = DIR_SUBDIR_CF.getBytes();
 
     //文件表列族
-    static final String OBJ_CONT_CF = "c";
-    static final byte[] OBJ_CONT_CF_BYTES = OBJ_CONT_CF.getBytes();
-    static final String OBJ_META_CF = "cf";
-    static final byte[] OBJ_META_CF_BYTES = OBJ_META_CF.getBytes();
+    public static final String OBJ_CONT_CF = "c";
+    public static final byte[] OBJ_CONT_CF_BYTES = OBJ_CONT_CF.getBytes();
+    public static final String OBJ_META_CF = "cf";
+    public static final byte[] OBJ_META_CF_BYTES = OBJ_META_CF.getBytes();
 
     //列名
-    static final byte[] DIR_SEQID_QUALIFIER = "u".getBytes();
-    static final byte[] OBJ_CONT_QUALIFIER = "c".getBytes();
-    static final byte[] OBJ_LEN_QUALIFIER = "l".getBytes();
-    static final byte[] OBJ_PROPS_QUALIFIER = "p".getBytes();
-    static final byte[] OBJ_MEDIATYPE_QUALIFIER = "m".getBytes();
+    public static final byte[] DIR_SEQID_QUALIFIER = "u".getBytes();
+    public static final byte[] OBJ_CONT_QUALIFIER = "c".getBytes();
+    public static final byte[] OBJ_LEN_QUALIFIER = "l".getBytes();
+    public static final byte[] OBJ_PROPS_QUALIFIER = "p".getBytes();
+    public static final byte[] OBJ_MEDIATYPE_QUALIFIER = "m".getBytes();
 
-    static final String FILE_STORE_ROOT = "/oss";
+    public static final String FILE_STORE_ROOT = "/oss";
     //上限值 大于此值存于hdfs,小于的话保存在hbase
-    static final int FILE_STORE_THRESHOLD = 20 * 1024 * 1024;
+    public static final int FILE_STORE_THRESHOLD = 20 * 1024 * 1024;
 
-    static final int OBJ_LIST_MAX_COUNT = 200;
-    static final String BUCKET_DIR_SEQ_TABLE = "oss_dir_seq";
-    static final String BUCKET_DIR_SEQ_CF = "s";
-    static final byte[] BUCKET_DIR_SEQ_CF_BYTES = BUCKET_DIR_SEQ_CF.getBytes();
-    static final byte[] BUCKET_DIR_SEQ_QUALIFIER = "s".getBytes();
+    public static final int OBJ_LIST_MAX_COUNT = 200;
+    public static final String BUCKET_DIR_SEQ_TABLE = "oss_dir_seq";
+    public static final String BUCKET_DIR_SEQ_CF = "s";
+    public static final byte[] BUCKET_DIR_SEQ_CF_BYTES = BUCKET_DIR_SEQ_CF.getBytes();
+    public static final byte[] BUCKET_DIR_SEQ_QUALIFIER = "s".getBytes();
 
-    static final FilterList OBJ_META_SCAN_FILTER = new FilterList(Operator.MUST_PASS_ONE);
+    public static final FilterList OBJ_META_SCAN_FILTER = new FilterList(Operator.MUST_PASS_ONE);
 
     static {
         try {
